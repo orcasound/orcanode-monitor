d196e5a0c9eded2667665b5c9b9cc0cbb2614156
Dave Thaler <dthaler1968@gmail.com>
2024-11-20 12:18:40 -0800

    Use separate containers for production vs staging vs development (#202)
    Fixes #197
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- OrcanodeMonitor/Data/OrcanodeMonitorContext.cs
+++ OrcanodeMonitor/Data/OrcanodeMonitorContext.cs
@@ -20,41 +20,47 @@ namespace OrcanodeMonitor.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? string.Empty;
+            if (environment == "Production")
+            {
+                environment = string.Empty;
+            }
+
             modelBuilder.Entity<MonitorState>()
-               .ToContainer("MonitorState")
+               .ToContainer(environment + "MonitorState")
                .Property(item => item.ID)
                .HasConversion<string>();
 
             modelBuilder.Entity<MonitorState>()
-                .ToContainer("MonitorState")
+                .ToContainer(environment + "MonitorState")
                 .HasPartitionKey(item=>item.ID);
 
             modelBuilder.Entity<Orcanode>()
-                .ToContainer("Orcanode")
+                .ToContainer(environment + "Orcanode")
                 .Property(item => item.PartitionValue)
                 .HasConversion<string>();
 
             modelBuilder.Entity<Orcanode>()
-                .ToContainer("Orcanode")
+                .ToContainer(environment + "Orcanode")
                 .Property(item => item.ID);
 
             modelBuilder.Entity<Orcanode>()
-                .ToContainer("Orcanode")
+                .ToContainer(environment + "Orcanode")
                 .Property(item => item.AudioStreamStatus)
                 .HasDefaultValue(OrcanodeOnlineStatus.Absent);
 
             modelBuilder.Entity<Orcanode>()
-                .ToContainer("Orcanode")
+                .ToContainer(environment + "Orcanode")
                 .HasPartitionKey(item => item.PartitionValue)
                 .HasKey(item=>item.ID);
 
             modelBuilder.Entity<OrcanodeEvent>()
-                .ToContainer("OrcanodeEvent")
+                .ToContainer(environment + "OrcanodeEvent")
                 .Property(item => item.Year)
                 .HasConversion<string>();
 
             modelBuilder.Entity<OrcanodeEvent>()
-                .ToContainer("OrcanodeEvent")
+                .ToContainer(environment + "OrcanodeEvent")
                 .HasPartitionKey(item => item.Year)
                 .HasOne(item => item.Orcanode)
                 .WithMany()
