37228bd136928f3ca383ed4ffc5979d4c1639785
Dave Thaler <dthaler1968@gmail.com>
2024-11-19 16:38:03 -0800

    Expose node name in events API for IFTTT
    Addresses part of #189
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- OrcanodeMonitor/Models/OrcanodeEvent.cs
+++ OrcanodeMonitor/Models/OrcanodeEvent.cs
@@ -28,6 +28,7 @@ namespace OrcanodeMonitor.Models
     {
         public OrcanodeIftttEventDTO(string id, string nodeName, string slug, string type, string value, DateTime timestamp, int severity)
         {
+            NodeName = nodeName;
             Slug = slug;
             Type = type;
             Value = value;
@@ -37,6 +38,8 @@ namespace OrcanodeMonitor.Models
         }
         [JsonPropertyName("slug")]
         public string Slug { get; private set; }
+        [JsonPropertyName("node_name")]
+        public string NodeName { get; private set; }
         [JsonPropertyName("type")]
         public string Type { get; private set; }
         [JsonPropertyName("value")]
