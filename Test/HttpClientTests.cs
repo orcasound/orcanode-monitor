19878ec4393dc0d9a351b5794b67866435400d34
Dave Thaler <dthaler1968@gmail.com>
2025-09-06 12:35:21 -0700

    Apply suggestions from code review

--- Test/HttpClientTests.cs
+++ Test/HttpClientTests.cs
@@ -12,65 +12,65 @@ namespace Test
         [TestMethod]
         public void FetcherHttpClientDisablesAutoRedirect()
         {
-            // Use reflection to access the private static HttpClient field
+            // Use reflection to access the private static HttpClient field.
             var httpClientField = typeof(Fetcher).GetField("_httpClient", BindingFlags.NonPublic | BindingFlags.Static);
             Assert.IsNotNull(httpClientField, "HttpClient field should exist");
 
             var httpClient = httpClientField.GetValue(null) as HttpClient;
             Assert.IsNotNull(httpClient, "HttpClient should not be null");
 
-            // Check HttpClient properties to verify it's configured properly
+            // Check HttpClient properties to verify it's configured properly.
             Assert.IsNotNull(httpClient.DefaultRequestHeaders, "DefaultRequestHeaders should not be null");
             
-            // Create a request and check the handler behavior
+            // Create a request and check the handler behavior.
             var request = new HttpRequestMessage(HttpMethod.Get, "https://httpbin.org/redirect/1");
             
-            // The HttpClient should be configured to not follow redirects
-            // We can verify this indirectly by checking that the client throws or returns non-success on redirect
+            // The HttpClient should be configured to not follow redirects.
+            // We can verify this indirectly by checking that the client throws or returns non-success on redirect.
             // Since we can't easily access the handler property due to internal implementation,
-            // we'll verify by attempting a redirect request and checking the response
+            // we'll verify by attempting a redirect request and checking the response.
             
-            // For now, just verify that the HttpClient instance exists and is properly initialized
+            // For now, just verify that the HttpClient instance exists and is properly initialized.
             Assert.IsNotNull(httpClient, "HttpClient should be properly initialized");
         }
 
         [TestMethod]
         public void MezmoFetcherHttpClientDisablesAutoRedirect()
         {
-            // Use reflection to access the private static HttpClient field
+            // Use reflection to access the private static HttpClient field.
             var httpClientField = typeof(MezmoFetcher).GetField("_httpClient", BindingFlags.NonPublic | BindingFlags.Static);
             Assert.IsNotNull(httpClientField, "HttpClient field should exist");
 
             var httpClient = httpClientField.GetValue(null) as HttpClient;
             Assert.IsNotNull(httpClient, "HttpClient should not be null");
 
-            // Check HttpClient properties to verify it's configured properly
+            // Check HttpClient properties to verify it's configured properly.
             Assert.IsNotNull(httpClient.DefaultRequestHeaders, "DefaultRequestHeaders should not be null");
             
-            // For now, just verify that the HttpClient instance exists and is properly initialized
+            // For now, just verify that the HttpClient instance exists and is properly initialized.
             Assert.IsNotNull(httpClient, "HttpClient should be properly initialized");
         }
 
         [TestMethod]
         public async Task TestRedirectBehaviorFetcher()
         {
-            // Test that HttpClient doesn't follow redirects by making a request to a redirect URL
+            // Test that HttpClient doesn't follow redirects by making a request to a redirect URL.
             var httpClientField = typeof(Fetcher).GetField("_httpClient", BindingFlags.NonPublic | BindingFlags.Static);
             var httpClient = httpClientField.GetValue(null) as HttpClient;
             
             try
             {
-                // Use httpbin redirect endpoint - it will return 302 if redirects are disabled
+                // Use httpbin redirect endpoint - it will return 302 if redirects are disabled.
                 var response = await httpClient.GetAsync("https://httpbin.org/redirect/1");
                 
-                // If auto redirect is disabled, we should get a 3xx response code
+                // If auto redirect is disabled, we should get a 3xx response code.
                 Assert.IsTrue((int)response.StatusCode >= 300 && (int)response.StatusCode < 400, 
                     $"Expected 3xx redirect response, but got {response.StatusCode}");
             }
             catch (HttpRequestException)
             {
-                // If we get an exception, that's also acceptable as some redirect scenarios might fail
-                // The important thing is that we don't automatically follow the redirect
+                // If we get an exception, that's also acceptable as some redirect scenarios might fail.
+                // The important thing is that we don't automatically follow the redirect.
                 Assert.IsTrue(true, "HttpRequestException is acceptable - indicates redirect wasn't followed");
             }
         }
@@ -78,23 +78,23 @@ namespace Test
         [TestMethod]
         public async Task TestRedirectBehaviorMezmoFetcher()
         {
-            // Test that HttpClient doesn't follow redirects by making a request to a redirect URL
+            // Test that HttpClient doesn't follow redirects by making a request to a redirect URL.
             var httpClientField = typeof(MezmoFetcher).GetField("_httpClient", BindingFlags.NonPublic | BindingFlags.Static);
             var httpClient = httpClientField.GetValue(null) as HttpClient;
             
             try
             {
-                // Use httpbin redirect endpoint - it will return 302 if redirects are disabled
+                // Use httpbin redirect endpoint - it will return 302 if redirects are disabled.
                 var response = await httpClient.GetAsync("https://httpbin.org/redirect/1");
                 
-                // If auto redirect is disabled, we should get a 3xx response code
+                // If auto redirect is disabled, we should get a 3xx response code.
                 Assert.IsTrue((int)response.StatusCode >= 300 && (int)response.StatusCode < 400, 
                     $"Expected 3xx redirect response, but got {response.StatusCode}");
             }
             catch (HttpRequestException)
             {
-                // If we get an exception, that's also acceptable as some redirect scenarios might fail
-                // The important thing is that we don't automatically follow the redirect
+                // If we get an exception, that's also acceptable as some redirect scenarios might fail.
+                // The important thing is that we don't automatically follow the redirect.
                 Assert.IsTrue(true, "HttpRequestException is acceptable - indicates redirect wasn't followed");
             }
         }
